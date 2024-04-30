967891994c845299398c03f2642bf71f9498bb8e
Peter Khondowe <167969372+PeterTe3@users.noreply.github.com>
2024-04-30 13:56:35 -0700

    Changes made to salary.php, app.blade.php and added Tailwind

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -1,11 +1,17 @@
 <?php
 
+// app/Models/Salary.php
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Salary extends Model
 {
-    use HasFactory;
+    protected $fillable = ['employee_id', 'salary', 'month', 'year']; // Fillable fields
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
